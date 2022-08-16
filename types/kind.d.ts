ea4a4235a8f0373d8403a4b5a54c74327ffdf1e9
Rod Vagg <rod@vagg.org>
2022-08-16 14:32:17 +1000

    feat!: publish as ESM-only

---
+++ types/kind.d.ts
@@ -0,0 +1,10 @@
+/**
+ * @typedef {'bool'|'string'|'bytes'|'int'|'float'|'list'|'map'|'null'|'link'} RepresentationKindString
+ */
+/**
+ * @param {any} obj
+ * @returns {RepresentationKindString}
+ */
+export function kind(obj: any): RepresentationKindString;
+export type RepresentationKindString = 'bool' | 'string' | 'bytes' | 'int' | 'float' | 'list' | 'map' | 'null' | 'link';
+//# sourceMappingURL=kind.d.ts.map
