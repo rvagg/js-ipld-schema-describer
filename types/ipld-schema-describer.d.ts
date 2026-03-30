2dff29fbbfb9c185cc59ddbd31fe20e734a32846
Rod Vagg <rod@vagg.org>
2026-03-31 09:47:00 +1100

    chore: update deps & upgrade to typescript 6

--- types/ipld-schema-describer.d.ts
+++ types/ipld-schema-describer.d.ts
@@ -1,8 +1,8 @@
 /**
- * @typedef {import('ipld-schema/schema-schema').Schema} Schema
- * @typedef {import('ipld-schema/schema-schema').TypeDefnLink} TypeDefnLink
- * @typedef {import('ipld-schema/schema-schema').TypeDefnList} TypeDefnList
- * @typedef {import('ipld-schema/schema-schema').TypeDefnMap} TypeDefnMap
+ * @typedef {import('@ipld/schema/schema-schema.js').Schema} Schema
+ * @typedef {import('@ipld/schema/schema-schema.js').TypeDefnLink} TypeDefnLink
+ * @typedef {import('@ipld/schema/schema-schema.js').TypeDefnList} TypeDefnList
+ * @typedef {import('@ipld/schema/schema-schema.js').TypeDefnMap} TypeDefnMap
  */
 /**
  * @param {any} obj
@@ -12,8 +12,8 @@ export function describe(obj: any): {
     schema: Schema;
     root: string;
 };
-export type Schema = import('ipld-schema/schema-schema').Schema;
-export type TypeDefnLink = import('ipld-schema/schema-schema').TypeDefnLink;
-export type TypeDefnList = import('ipld-schema/schema-schema').TypeDefnList;
-export type TypeDefnMap = import('ipld-schema/schema-schema').TypeDefnMap;
+export type Schema = import("@ipld/schema/schema-schema.js").Schema;
+export type TypeDefnLink = import("@ipld/schema/schema-schema.js").TypeDefnLink;
+export type TypeDefnList = import("@ipld/schema/schema-schema.js").TypeDefnList;
+export type TypeDefnMap = import("@ipld/schema/schema-schema.js").TypeDefnMap;
 //# sourceMappingURL=ipld-schema-describer.d.ts.map
